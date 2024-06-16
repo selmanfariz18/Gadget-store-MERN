ade1a7a30f2041494be2630098fc9620f179a2e9
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-06-16 07:29:35 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
 //config env
-dotenv.config()
+dotenv.config();
+
+//database config
+connectDB();
 
 //rest object
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
 
 //rest api
 app.get("/", (req, res) => {
@@ -18,5 +27,5 @@ const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`.bgCyan.white);
+    console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
