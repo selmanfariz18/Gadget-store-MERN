ef824c38e4737a45f94bdc77b5162f6daae5c77c
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-06-28 22:42:29 +0530

    routes sett in react

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer comp</h1>
+    <div className="bg-dark text-light p-3">
+      <h4 className="text-center">All Right Reserved &copy; fariz</h4>
     </div>
   );
 };
