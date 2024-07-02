8f4dd7623c56f22d0133afe993d31ed9f4ed4533
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-07-02 22:29:48 +0530

    toastify added for submission

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,50 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./../../components/Layout/Layout";
+import { toast } from "react-toastify";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  //form function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, phone, address);
+    toast.success("Register Successfully!");
+  };
+
   return (
     <Layout title={"Register"}>
       <div className="register">
         <h1>Register page</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <input
               type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Name"
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Email"
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="form-control"
               id="exampleInputPassword1"
               placeholder="Password"
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Phone"
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Address"
+              required
             />
           </div>
           <button type="submit" className="btn btn-primary">
