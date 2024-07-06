55957d115296e6001f24d548fd3a7371a18fb684
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-07-06 19:58:18 +0530

    location used to track previous page before login

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Layout from "./../../components/Layout/Layout";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useAuth } from "../../context/auth";
 
@@ -11,6 +11,7 @@ const Login = () => {
   const [auth, setAuth] = useAuth();
 
   const navigate = useNavigate();
+  const locaction = useLocation();
 
   //form function
   const handleSubmit = async (e) => {
@@ -31,7 +32,7 @@ const Login = () => {
           token: res.data.token,
         });
         localStorage.setItem("auth", JSON.stringify(res.data));
-        navigate("/");
+        navigate(locaction.state || "/");
       } else {
         toast.error(res.data.message);
       }
