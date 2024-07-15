3633e18b2880dac5e2c9e3ad8e58b4a7036d96a3
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-07-15 20:54:14 +0530

    user dashboard created

--- client/src/pages/Admin/Users.js
+++ client/src/pages/Admin/Users.js
@@ -1,23 +1,17 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
-import { useAuth } from "../../context/auth";
 
-const AdminDashboard = () => {
-  const [auth] = useAuth();
+const Users = () => {
   return (
-    <Layout>
+    <Layout title={"Dashboard - All users"}>
       <div className="container-fluid m-3 p-3">
         <div className="row">
           <div className="col-md-3">
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <div className="card w-75 p-3">
-              <h3>Admin Name: {auth?.user?.name}</h3>
-              <h3>Admin Email: {auth?.user?.email}</h3>
-              <h3>Admin Contact: {auth?.user?.phone}</h3>
-            </div>
+            <h1>All users</h1>
           </div>
         </div>
       </div>
@@ -25,4 +19,4 @@ const AdminDashboard = () => {
   );
 };
 
-export default AdminDashboard;
+export default Users;
--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,24 +1,17 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
 import UserMenu from "../../components/Layout/UserMenu";
-import { useAuth } from "../../context/auth";
 
-const Dashboard = () => {
-  const [auth] = useAuth();
+const Profile = () => {
   return (
-    <Layout title={"Dashboard"}>
+    <Layout title={"Profile"}>
       <div className="container-flui p-3 m-3">
         <div className="row">
           <div className="col-md-3">
             <UserMenu />
           </div>
           <div className="col-md-9">
-            <div className="card w-75 p-3">
-              <h3>{auth?.user?.name}</h3>
-              <h3>{auth?.user?.email}</h3>
-              <h3>{auth?.user?.address}</h3>
-              <h3>{auth?.user?.phone}</h3>
-            </div>
+            <h1>Profile</h1>
           </div>
         </div>
       </div>
@@ -26,4 +19,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default Profile;
